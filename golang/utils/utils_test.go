ff411a61ce5c96a46556c32d3360af2073232d77
ZhaoJiaJin <zhaojin0905@gmail.com>
2020-07-26 16:30:53 +0100

    change test case

--- golang/utils/utils_test.go
+++ golang/utils/utils_test.go
@@ -3,19 +3,19 @@ package utils
 import(
     "testing"
     "github.com/stretchr/testify/assert"
+    "math"
 )
 
 func TestCalN(t *testing.T){
-    assert.Equal(t,  48.0,CalN(1), "CalN(1) is wrong.")
-    assert.Equal(t, 16 *12.0,CalN(2),  "CalN(2) is wrong.")
-    assert.Equal(t, 16 * 4 *12.0, CalN(3), "CalN(3) is wrong.")
+    assert.Equal(t,  48,CalN(1), "CalN(1) is wrong.")
+    assert.Equal(t, 16 *12,CalN(2),  "CalN(2) is wrong.")
+    assert.Equal(t, 16 * 4 *12, CalN(3), "CalN(3) is wrong.")
 }
 
 func TestMostSqure(t *testing.T){
-    x,y := MostSqure(4)
-    assert.Equal(t, 2, x, "MostSqure Wrong")
-    assert.Equal(t, 2, y, "MostSqure Wrong")
-    x,y = MostSqure(6)
-    assert.Equal(t, 2, x, "MostSqure Wrong")
-    assert.Equal(t, 3, y, "MostSqure Wrong")
+    for i:=1; i < 12; i ++{
+        x,y := MostSqure(CalN(i))
+        assert.Equal(t, int(math.Pow(2,float64(i))*3), x, "MostSqure Wrong")
+        assert.Equal(t, int(math.Pow(2,float64(i))*4), y, "MostSqure Wrong")
+    }
 }
